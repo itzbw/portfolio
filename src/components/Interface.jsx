eb13ece6cbc4d4603fe54645927522686447b6fc
bw <thebw.42@pm.me>
2024-09-16 14:29:28 +0200

    update: skill section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,13 +1,14 @@
 import { motion, transform } from "framer-motion";
 import { atom, useAtom } from "jotai";
 import { currentProjectAtom, projects } from "./Projects";
+
 const Section = (props) => {
   const { children } = props;
 
   return (
     <motion.section
       className={`
-    h-screen w-screen p-8 max-w-screen-2xl mx-auto
+    h-screen w-screen p-8 max-w-screen-2xl
     flex flex-col items-start justify-center
     `}
       initial={{ opacity: 0, y: 50 }}
@@ -77,76 +78,188 @@ const AboutSection = (props) => {
   );
 };
 
+// const skills = [
+//   {
+//     title: "Three.js",
+//     level: 50,
+//   },
+//   {
+//     title: "JS",
+//     level: 60,
+//   },
+//   {
+//     title: "Docker",
+//     level: 30,
+//   },
+//   {
+//     title: "C",
+//     level: 60,
+//   },
+//   {
+//     title: "C++",
+//     level: 40,
+//   },
+// ];
+
+// const languages = [
+//   {
+//     title: "English",
+//     level: 90,
+//   },
+//   {
+//     title: "French",
+//     level: 70,
+//   },
+//   {
+//     title: "Traditional Chinese",
+//     level: 100,
+//   },
+//   {
+//     title: "Japanese",
+//     level: 80,
+//   },
+//   {
+//     title: "Simplified Chinese",
+//     level: 50,
+//   },
+// ];
+
+// const SkillList = (props) =>
+//   props.items.map((skill, index) => (
+//     <div className="w-64" key={index}>
+//       <motion.h3
+//         className="text-xl font-bold text-white"
+//         initial={{ opacity: 0 }}
+//         variants={{
+//           visible: {
+//             opacity: 1,
+//             transition: { duration: 1, delay: 1 + index * 0.2 },
+//           },
+//         }}
+//       >
+//         {skill.title}
+//       </motion.h3>
+//       <div className="h-2 w-full rounded-full mt-1">
+//         <motion.div
+//           className="h-full bg-gray-300 rounded-full"
+//           style={{ width: `${skill.level}%` }}
+//           initial={{ scaleX: 0, originX: 0 }}
+//           variants={{
+//             visible: {
+//               scaleX: 1,
+//               transition: { duration: 1, delay: 1 + index * 0.2 },
+//             },
+//           }}
+//         />
+//       </div>
+//     </div>
+//   ));
+
+// const SkillSection = () => {
+//   return (
+//     <Section>
+//       <motion.div whileInView={"visible"}>
+//         <motion.h2
+//           whileInView={"visible"}
+//           initial={{ opacity: 0 }}
+//           variants={{
+//             visible: {
+//               opacity: 1,
+//               transition: { duration: 1, delay: 1 },
+//             },
+//           }}
+//           className="bright-gradient text-5xl h-14 font-bold text-white"
+//         >
+//           Skills
+//         </motion.h2>
+//         <div className="mt-6 space-y-2">
+//           <SkillList items={skills} />
+//         </div>
+//       </motion.div>
+//       <br />
+//       <motion.div whileInView={"visible"}>
+//         <motion.h2
+//           whileInView={"visible"}
+//           initial={{ opacity: 0 }}
+//           variants={{
+//             visible: {
+//               opacity: 1,
+//               transition: { duration: 1, delay: 1.5 },
+//             },
+//           }}
+//           className="bright-gradient text-5xl h-14 font-bold text-white mt-8"
+//         >
+//           Languages
+//         </motion.h2>
+//         <div className="mt-8 space-y-2">
+//           <SkillList items={languages} />
+//         </div>
+//       </motion.div>
+//     </Section>
+//   );
+// };
+
 const skills = [
-  {
-    title: "Three.js",
-    level: 50,
-  },
-  {
-    title: "JS",
-    level: 60,
-  },
-  {
-    title: "Docker",
-    level: 30,
-  },
-  {
-    title: "C",
-    level: 60,
-  },
-  {
-    title: "C++",
-    level: 40,
-  },
+  { title: "HTML" },
+  { title: "CSS" },
+  { title: "JavaScript" },
+  { title: "Three.js" },
+  { title: "React" },
+  { title: "Node.js" },
+  { title: "C" },
 ];
 
 const languages = [
-  {
-    title: "English",
-    level: 90,
-  },
-  {
-    title: "French",
-    level: 70,
-  },
-  {
-    title: "Traditional Chinese",
-    level: 100,
-  },
-  {
-    title: "Japanese",
-    level: 80,
-  },
-  {
-    title: "Simplified Chinese",
-    level: 50,
-  },
+  { title: "English", level: 90 },
+  { title: "French", level: 70 },
+  { title: "Traditional Chinese", level: 100 },
+  { title: "Japanese", level: 80 },
+  { title: "Simplified Chinese", level: 50 },
 ];
 
-const SkillList = (props) =>
-  props.items.map((skill, index) => (
-    <div className="w-64" key={index}>
+const SkillList = ({ items }) =>
+  items.map((skill, index) => (
+    <motion.div
+      key={index}
+      className="text-xl font-bold text-white mb-2 mr-4"
+      initial={{ opacity: 0, x: -20 }}
+      variants={{
+        visible: {
+          opacity: 1,
+          x: 0,
+          transition: { duration: 0.5, delay: 0.1 * index },
+        },
+      }}
+    >
+      {skill.title}
+    </motion.div>
+  ));
+
+const LanguageList = ({ items }) =>
+  items.map((lang, index) => (
+    <div className="w-full max-w-sm mb-4" key={index}>
       <motion.h3
         className="text-xl font-bold text-white"
-        initial={{ opacity: 0 }}
+        initial={{ opacity: 0, x: -20 }}
         variants={{
           visible: {
             opacity: 1,
-            transition: { duration: 1, delay: 1 + index * 0.2 },
+            x: 0,
+            transition: { duration: 0.5, delay: 0.2 * index },
           },
         }}
       >
-        {skill.title}
+        {lang.title}
       </motion.h3>
-      <div className="h-2 w-full rounded-full mt-1">
+      <div className="h-2 w-full bg-gray-700 rounded-full mt-1">
         <motion.div
-          className="h-full bg-gray-300 rounded-full"
-          style={{ width: `${skill.level}%` }}
+          className="h-full bg-white rounded-full"
+          style={{ width: `${lang.level}%` }}
           initial={{ scaleX: 0, originX: 0 }}
           variants={{
             visible: {
               scaleX: 1,
-              transition: { duration: 1, delay: 1 + index * 0.2 },
+              transition: { duration: 1, delay: 0.2 * index },
             },
           }}
         />
@@ -154,44 +267,83 @@ const SkillList = (props) =>
     </div>
   ));
 
+// const SkillSection = () => {
+//   return (
+//     <section>
+//       <motion.div whileInView="visible" className="w-full">
+//         <motion.h2
+//           initial={{ opacity: 0, x: -20 }}
+//           variants={{
+//             visible: {
+//               opacity: 1,
+//               x: 0,
+//               transition: { duration: 1 },
+//             },
+//           }}
+//           className="text-5xl font-bold text-white mb-6 "
+//         >
+//           Skills
+//         </motion.h2>
+//         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-10 ">
+//           <SkillList items={skills} />
+//         </div>
+
+//         <motion.h2
+//           initial={{ opacity: 0, x: -20 }}
+//           variants={{
+//             visible: {
+//               opacity: 1,
+//               x: 0,
+//               transition: { duration: 1, delay: 0.5 },
+//             },
+//           }}
+//           className="text-5xl font-bold text-white mb-6 mt-10"
+//         >
+//           Languages
+//         </motion.h2>
+//         <div className="space-y-2">
+//           <LanguageList items={languages} />
+//         </div>
+//       </motion.div>
+//     </section>
+//   );
+// };
 const SkillSection = () => {
   return (
-    <Section>
-      <motion.div whileInView={"visible"}>
+    <Section className="py-10 px-4 sm:px-6 lg:px-8">
+      <motion.div whileInView="visible" className="w-full max-w-xs">
         <motion.h2
-          whileInView={"visible"}
-          initial={{ opacity: 0 }}
+          initial={{ opacity: 0, y: 50 }}
           variants={{
             visible: {
               opacity: 1,
-              transition: { duration: 1, delay: 1 },
+              y: 0,
+              transition: { duration: 1 },
             },
           }}
-          className="bright-gradient text-5xl h-14 font-bold text-white"
+          className="text-5xl font-bold text-white mb-6"
         >
           Skills
         </motion.h2>
-        <div className="mt-6 space-y-2">
+        <div className="flex flex-wrap gap-y-2 mb-10 w-full">
           <SkillList items={skills} />
         </div>
-      </motion.div>
-      <br />
-      <motion.div whileInView={"visible"}>
+
         <motion.h2
-          whileInView={"visible"}
-          initial={{ opacity: 0 }}
+          initial={{ opacity: 0, y: 50 }}
           variants={{
             visible: {
               opacity: 1,
-              transition: { duration: 1, delay: 1.5 },
+              y: 0,
+              transition: { duration: 1, delay: 0.5 },
             },
           }}
-          className="bright-gradient text-5xl h-14 font-bold text-white mt-8"
+          className="text-5xl font-bold text-white mb-6 mt-10"
         >
           Languages
         </motion.h2>
-        <div className="mt-8 space-y-2">
-          <SkillList items={languages} />
+        <div className="space-y-2">
+          <LanguageList items={languages} />
         </div>
       </motion.div>
     </Section>
